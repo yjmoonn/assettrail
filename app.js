c138f27afed6e462b68f4686e38a44486d90da89
문영재 <170986833+yjmoonn@users.noreply.github.com>
2026-05-23 23:17:24 +0900

    Classify savings in account analysis

--- app.js
+++ app.js
@@ -685,6 +685,7 @@ function regionLabel(asset) {
 
 function accountClassLabel(asset) {
   const text = `${asset.account || ""} ${asset.name || ""} ${asset.note || ""}`.toLowerCase();
+  if (/적금/i.test(text)) return "적금";
   if (/(연금|irp|퇴직)/i.test(text)) return "연금계좌";
   if (asset.account) return "일반계좌";
   return "계좌 미지정";
